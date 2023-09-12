acc31ff8302d1ee5d85615374d32e94a48345f39
shabna <shabnavahab662@gmail.com>
2023-09-11 23:52:46 -0700

    login and signup

--- cakeshop/urls.py
+++ cakeshop/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
     path('login/',views.login),
+    path('signup/',views.signup),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
