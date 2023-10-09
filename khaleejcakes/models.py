b638535ca90f2f4c3985cb0e53d052f8b9bddca8
shabna <shabnavahab662@gmail.com>
2023-10-09 11:35:23 +0530

    athontication ok

--- khaleejcakes/models.py
+++ khaleejcakes/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class table_user(models.Model):
+    username=models.CharField(max_length=50)
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    gender=models.CharField(max_length=50)
+    phone=models.IntegerField()
+    email=models.CharField(max_length=50)
+    address=models.CharField(max_length=50)
+    district=models.CharField(max_length=50)
+    state=models.CharField(max_length=50)
+
+    class Meta:
+        db_table='table_user'
 
 
 # Create your models here.
